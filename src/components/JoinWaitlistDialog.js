df03ab0ea61e3a32b29367bd5e56e1a443157ec5
olepychev <oleksandrpychev@gmail.com>
2022-09-20 09:58:36 +0900

    waitlist dialog layout updated

--- src/components/JoinWaitlistDialog.js
+++ src/components/JoinWaitlistDialog.js
@@ -3,14 +3,42 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 // materials
-import { useTheme } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
+import { useTheme, withStyles } from '@material-ui/core/styles';
+import { Typography, Stack } from '@material-ui/core';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 import Image from './Image';
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2)
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500]
+  }
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
 
 JoinWaitlistDialog.propTypes = {
   isOpen: PropTypes.bool,
@@ -40,26 +68,43 @@ export default function JoinWaitlistDialog({ isOpen, onClose }) {
         sx={{
           '& .MuiPaper-root': {
             width: 400,
-            height: 520,
-            [theme.breakpoints.up('md')]: { width: 500 }
+            height: 530,
+            [theme.breakpoints.up('md')]: { width: 500, height: 550 }
           }
         }}
       >
-        <DialogContent sx={{ overflowY: 'hidden' }}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose} />
+        <DialogContent
+          sx={{ padding: 0, marginTop: -4, [theme.breakpoints.up('md')]: { overflowY: 'hidden', padding: 2 } }}
+        >
           <Image src="/static/landing/astronaut.png" sx={{ width: 120, height: 120, margin: 'auto' }} />
-          <Typography variant="h4" sx={{ textAlign: 'center', my: 2 }}>
-            Join limited Presale Waitlist
+          <Typography variant="h3" sx={{ textAlign: 'center', my: 1 }}>
+            Join Presale Waitlist
           </Typography>
-          <div className="launchlist-widget" data-key-id="31PxJ8" data-height="250px" />
+          <Stack spacing={0.5}>
+            <Typography variant="body2" sx={{ textAlign: 'center' }}>
+              Early Bird Airdrop Schedule
+              <span role="img" aria-label="Panda" style={{ fontSize: '25px' }}>
+                🛸
+              </span>
+            </Typography>
+
+            <Typography variant="body2" sx={{ textAlign: 'center', fontFamily: 'BarlowRegular' }}>
+              <b>Milestone 1</b>: First 10,000 users get 100 APED
+            </Typography>
+            <Typography variant="body2" sx={{ textAlign: 'center', fontFamily: 'BarlowRegular' }}>
+              <b>Milestone 2</b>: First 20,000 users get 50 APED
+            </Typography>
+            <Typography variant="body2" sx={{ textAlign: 'center', fontFamily: 'BarlowRegular' }}>
+              <b>Milestone 3</b>: First 50,000 users get 20 APED
+            </Typography>
+          </Stack>
+
+          <div className="launchlist-widget" data-key-id="31PxJ8" data-height="220px" />
           <Helmet>
             <script src="https://getlaunchlist.com/js/widget.js" type="text/javascript" defer />
           </Helmet>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} variant="contained" color="secondary" autoFocus>
-            Cancel
-          </Button>
-        </DialogActions>
       </Dialog>
     </div>
   );
