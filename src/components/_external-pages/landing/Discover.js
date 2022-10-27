d52f4544376f7299056e37fa5c9bdac5838f25aa
olepychev <oleksandrpychev@gmail.com>
2022-10-28 05:54:05 +0900

    table removed on discover

--- src/components/_external-pages/landing/Discover.js
+++ src/components/_external-pages/landing/Discover.js
@@ -1,21 +1,12 @@
-/* eslint-disable prefer-destructuring */
-/* eslint-disable camelcase */
-/* eslint-disable dot-notation */
 import { motion } from 'framer-motion';
 import React from 'react';
 import Slider from 'react-slick';
 
 // material
-import { experimentalStyled as styled, withStyles, useTheme } from '@material-ui/core/styles';
-import { Typography, Stack, Button, Container, Box, useMediaQuery } from '@material-ui/core';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { experimentalStyled as styled, useTheme } from '@material-ui/core/styles';
+import { Typography, Stack, Container, Box, useMediaQuery } from '@material-ui/core';
 //
-import { varWrapEnter, varFadeInUp } from '../../animate';
-import { MHidden } from '../../@material-extend';
+import { varWrapEnter } from '../../animate';
 
 import Image from '../../Image';
 // const finnhub = require('finnhub');
@@ -23,39 +14,6 @@ import Image from '../../Image';
 // const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 // api_key.apiKey = 'c9r0paiad3ibg4fjln3g'; // Replace this
 // ----------------------------------------------------------------------
-
-const TableRowStyle = withStyles((theme) => ({
-  root: {
-    '& .MuiTableCell-root': {
-      color: theme.palette.grey[0],
-      fontFamily: 'BarlowExtraBoldItalic',
-      [theme.breakpoints.down('md')]: {
-        padding: 0,
-        paddingTop: 5,
-        paddingBottom: 5
-      }
-    },
-    '& .MuiTableCell-head': {
-      backgroundColor: '#1a1826',
-      borderBottom: `solid 1px ${theme.palette.grey[500_48]}`,
-      '&:first-child': {
-        [theme.breakpoints.down('md')]: {
-          paddingLeft: theme.spacing(1)
-        }
-      }
-    },
-    [theme.breakpoints.down('md')]: {
-      '& .MuiTypography-subtitle1': {
-        fontSize: 13
-      }
-    },
-    '& .MuiTableCell-root:first-of-type, .MuiTableCell-root:last-of-type': {
-      boxShadow: 'none',
-      borderBottomLeftRadius: theme.spacing(0),
-      borderBottomRightRadius: theme.spacing(0)
-    }
-  }
-}))(TableRow);
 
 const RootStyle = styled(motion.div)(({ theme }) => ({
   padding: theme.spacing(10, 0),
@@ -69,18 +27,6 @@ const ContentStyle = styled((props) => <Stack spacing={5} {...props} />)(({ them
   paddingBottom: theme.spacing(5),
   zIndex: 10
 }));
-
-function createData(markets, lastPrice, change, pChange) {
-  return { markets, lastPrice, change, pChange };
-}
-
-const rows = [
-  createData('Bitcoin', 1.08064, 1.08064, 0.4),
-  createData('Solonar', 1.08064, 1.08064, 0.4),
-  createData('Ethereum', 1.08064, 1.08064, 0.4),
-  createData('Binance', 1.08064, 1.08064, 0.4),
-  createData('Cardno', 1.08064, 1.08064, 0.4)
-];
 
 const discovers = [
   {
@@ -110,51 +56,6 @@ const discovers = [
 ];
 
 // ----------------------------------------------------------------------
-
-const TableContent = () => (
-  <Table
-    aria-label="simple table"
-    sx={{ border: (theme) => `solid 1px ${theme.palette.grey[500_48]}`, borderRadius: '9px', borderCollapse: 'unset' }}
-  >
-    <TableHead>
-      <TableRowStyle>
-        <TableCell>MARKETS</TableCell>
-        <TableCell align="center">LAST PRICE</TableCell>
-        <TableCell align="center">CHANGE</TableCell>
-        <TableCell align="center">% CHNAGE</TableCell>
-      </TableRowStyle>
-    </TableHead>
-    <TableBody>
-      {rows.map((row, i) => (
-        <TableRowStyle key={i}>
-          <TableCell component="th" scope="row" sx={{ paddingLeft: '10px !important' }}>
-            <Stack direction="row" spacing={0.5} alignItems="center">
-              <Box component="img" src={`/static/landing/assets-${i + 1}.svg`} />
-              <Typography variant="subtitle1" color="common.white">
-                {row.markets}
-              </Typography>
-            </Stack>
-          </TableCell>
-          <TableCell align="center">
-            <Typography variant="subtitle1" color="common.white">
-              {row.lastPrice}
-            </Typography>
-          </TableCell>
-          <TableCell align="center">
-            <Typography variant="subtitle1" color={i === 2 || i === 4 ? 'error.main' : 'primary.main'}>
-              {row.change}
-            </Typography>
-          </TableCell>
-          <TableCell align="center">
-            <Typography variant="subtitle1" color={i === 2 || i === 4 ? 'error.main' : 'primary.main'}>
-              {row.pChange}
-            </Typography>
-          </TableCell>
-        </TableRowStyle>
-      ))}
-    </TableBody>
-  </Table>
-);
 
 export default function Discover() {
   const theme = useTheme();
@@ -280,44 +181,6 @@ export default function Discover() {
             {renderSlides()}
           </Slider>
         </Box>
-
-        <Container sx={{ position: 'relative' }}>
-          <ContentStyle>
-            <Stack direction="row" spacing={3} sx={{ overflowX: 'auto' }}>
-              <TableContent />
-              <MHidden width="mdDown">
-                <TableContent />
-              </MHidden>
-            </Stack>
-
-            <Box m={4} />
-            <motion.div variants={varFadeInUp}>
-              <Button className="aped-button" variant="contained" target="_self" href="#">
-                VIEW ALL
-              </Button>
-            </motion.div>
-          </ContentStyle>
-          <Image
-            src="/static/landing/discover_right.png"
-            sx={{
-              position: 'absolute',
-              right: 0,
-              bottom: -120,
-              zIndex: 1,
-              [theme.breakpoints.down('md')]: { width: 136, height: 111, right: -40 }
-            }}
-          />
-          <Image
-            src="/static/landing/discover_left.png"
-            sx={{
-              position: 'absolute',
-              left: 0,
-              bottom: -120,
-              zIndex: 1,
-              [theme.breakpoints.down('md')]: { width: 136, height: 111, left: -40 }
-            }}
-          />
-        </Container>
       </RootStyle>
     </>
   );
